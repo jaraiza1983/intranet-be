0c62ea3b02cd4f2c0570170c8a33b30bd070e60e
Jonathan Araiza <jaraiza1983@gmail.com>
2025-03-17 09:45:18 -0700

    Agregando campos adicionales a la entidad usuario

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -30,9 +30,32 @@ export class User {
     })
     roles: string[];
 
+    @Column('text')
+    username: string;
+
+    @Column('text',{
+        default: '-'
+    })
+    first_name: string;
+
+    @Column('text',{
+        default: '-'
+    })
+    last_name: string;
+
+    @Column('text')
+    occupation?: string;
+
+    @Column('text')
+    companyName?:string;
+
+    @Column('text')
+    phone?:string;
+
     @BeforeInsert()
     checkFieldsBeforeInsert() {
         this.email = this.email.toLocaleLowerCase().trim();
+        this.username = this.email;
     }
 
     @BeforeUpdate()
