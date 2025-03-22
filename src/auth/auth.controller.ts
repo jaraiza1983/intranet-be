50ddecd731f224e2bf4ef043010a0c3ce362c4e0
Jonathan Araiza <jaraiza1983@gmail.com>
2025-03-22 10:09:05 -0700

    Desarrollando el Auth() decorator para unificar decoradores (Decorator Composition)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,8 +7,9 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
-import { RoleProtected } from './decorators/role-protected/role-protected.decorator';
+import { RoleProtected } from './decorators/role-protected.decorator';
 import { ValidRoles } from './interfaces/valid-roles';
+import { Auth } from './decorators/auth.decorator';
 
 
 @Controller('auth')
@@ -35,12 +36,20 @@ export class AuthController {
     return this.authService.user(authUserDto);
   }
 
-  @Get('test')
+  @Get('test1')
   @UseGuards( AuthGuard(), UserRoleGuard )
   @RoleProtected(ValidRoles.admin, ValidRoles.superUser)
-  getAuthUserTest(
+  getAuthUserTest1(
 
   ) {
-    return {hola: "mundo"}
+    return {test: "test 1"}
+  }
+
+  @Get('test2')
+  @Auth(ValidRoles.admin)
+  getAuthUserTest2(
+
+  ) {
+    return {test: "test 2"}
   }
 }
